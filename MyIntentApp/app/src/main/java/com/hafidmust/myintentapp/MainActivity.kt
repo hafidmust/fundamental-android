6ea08f4718146f23d70f1149c4a779bb12a08db3
Hafid <hafidalimustaqim13@gmail.com>
2022-01-26 12:06:18 +0700

    finish latihan explicit intent

--- MyIntentApp/app/src/main/java/com/hafidmust/myintentapp/MainActivity.kt
+++ MyIntentApp/app/src/main/java/com/hafidmust/myintentapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.hafidmust.myintentapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMoveActivity = findViewById<Button>(R.id.btn_move)
+
+        btnMoveActivity.setOnClickListener {
+            val moveIntent = Intent(this, MoveActivity::class.java)
+            startActivity(moveIntent)
+
+        }
     }
 }
