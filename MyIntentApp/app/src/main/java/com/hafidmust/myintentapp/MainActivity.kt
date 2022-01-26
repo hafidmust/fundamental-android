e316875ea896b339419a635fae28e4c365729b69
Hafid <hafidalimustaqim13@gmail.com>
2022-01-26 20:42:46 +0700

    finish latihan mengirim data pada intent

--- MyIntentApp/app/src/main/java/com/hafidmust/myintentapp/MainActivity.kt
+++ MyIntentApp/app/src/main/java/com/hafidmust/myintentapp/MainActivity.kt
@@ -13,11 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnMoveActivity = findViewById<Button>(R.id.btn_move)
+        val btnMoveWithDataActivity = findViewById<Button>(R.id.btn_move_with_data)
 
         btnMoveActivity.setOnClickListener {
             val moveIntent = Intent(this, MoveActivity::class.java)
             startActivity(moveIntent)
+        }
 
+        btnMoveWithDataActivity.setOnClickListener {
+            val moveWithData = Intent(this, MoveWithDataActivity::class.java)
+            moveWithData.putExtra(MoveWithDataActivity.EXTRA_NAME, "hafid")
+            moveWithData.putExtra(MoveWithDataActivity.EXTRA_AGE, 20)
+            startActivity(moveWithData)
         }
     }
 }
